0f494d95b95286d86e52383ae9342f46ce81100a
Emils <emilsberzs@hotmail.com>
2023-02-16 14:44:33 +0000

    Inititial commit. Created files, written HTML skeleton, CSS and JS bullet points for work to be done

---
+++ library.js
@@ -0,0 +1,5 @@
+//Write code to retrieve values from form after user clicks submit.
+
+//Write code to populate Book card with retrieved data.
+
+//Add buttons for Deleting the book, and for toggling read/not read
